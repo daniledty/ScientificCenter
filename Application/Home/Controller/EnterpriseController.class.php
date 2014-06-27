ef7bf4f9cb480c9401ed9299ebd412fd5f6b031e
dingtaoyong <1204338713@qq.com>
2014-06-27 20:51:22 +0800

    commit

---
+++ Application/Home/Controller/EnterpriseController.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class EnterpriseController extends Controller {
+    public function index(){
+        $this->display();
+    }
+    public function register(){
+        $this->display();
+    }
+    
+    public function search(){
+        $this->display();
+    }
+    
+    public function publish(){
+        $this->display();
+    }
+    
+    public function requirement() {
+        $this->display();
+    }
+}
---
+++ Application/Home/Controller/ExpertController.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ExpertController extends Controller {
+    public function index(){
+		$this->display();
+    }
+    public function register(){
+		$this->display();
+	}
+	public function demand_seek(){
+		$this->display();
+	}
+	public function patent_issue(){
+		$this->display();
+	}
+	public function patent_info(){
+		$this->display();
+	}
+	public function demand_info(){
+		$this->display();
+	}
+}
+
+
