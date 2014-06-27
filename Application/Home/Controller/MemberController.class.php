06e32481795549f87e21809936fde6a1a8a4195c
dingtaoyong <1204338713@qq.com>
2014-06-27 21:10:35 +0800

    commit

---
+++ Application/Home/Controller/MemberController.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class MemberController extends Controller {
+    public function index(){
+		$this->display();
+    }
+    public function register(){
+		$this->display();
+	}
+	public function reservation(){
+		$this->display();
+	}
+	public function reservation_info(){
+		$this->display();
+	}
+	
+}
+
+
